935270cc5c8d85b79792d502367a28c9d48264c7
TheCedarPrince <jacobszelko@gmail.com>
2021-10-26 15:48:46 -0400

    Added docstrings

--- julia/src/atlasUtilities.jl
+++ julia/src/atlasUtilities.jl
@@ -1,27 +1,26 @@
-#=
+"""
 
-get_concept
-
-Takes a valid OMOP Concept ID and returns a list of concept fields
-  @param id Integer representing an OMOP Concept ID
-
-  @return list Fields related to the concept
-
-  @examples
-  get_concept(436665)
-
- @export
-
-=#
-function get_atlas_concept(
+get_atlas_concept(
     id::Int;
     isExcluded::Bool = false,
     includeDescendants::Bool = true,
-    includeMapped::Bool = true,
+    includeMapped::Bool = true
 )
-    get_atlas_concept(string(id); isExcluded, includeDescendants, includeMapped)
-end
 
+Takes a valid OMOP Concept ID and returns vocabulary concept data.
+
+# Arguments
+
+- `id::Int` - valid OMOP Concept ID
+- `isExcluded::Bool` - Exclude this concept (and any of its descendants if selected) from the concept set
+- `includeDescendants::Bool` - Consider not only this concept, but also all of its descendants
+- `includeMapped::Bool` - Allow to search for non-standard concepts
+
+# Returns
+
+- `obj::JSON3.Object` - results in a JSON3 object representing the ATLAS concept
+
+"""
 function get_atlas_concept(
     id::String;
     isExcluded::Bool = false,
@@ -45,16 +44,34 @@ function get_atlas_concept(
 
 end
 
+function get_atlas_concept(
+    id::Int;
+    isExcluded::Bool = false,
+    includeDescendants::Bool = true,
+    includeMapped::Bool = true,
+)
+    get_atlas_concept(string(id); isExcluded, includeDescendants, includeMapped)
+end
 
-#=
 
-get_cohort
+"""
 
-=#
-function get_atlas_concept_set(condition_file::String)
-    file_contents = read(condition_file, String) 
+	get_atlas_concept_set(contents::String)
+
+Gets a list of concepts for a given concept set definition.
+
+# Arguments
+
+- `contents::String` - a string representation of the concept set query. **Should be a raw string of JSON input and not a JSON object.**
+
+# Returns
+
+- `list::Int` - list of OMOP Concept ids
+
+"""
+function get_atlas_concept_set(contents::String)
     path = "https://atlas-demo.ohdsi.org/WebAPI/vocabulary/resolveConceptSetExpression/"
-    obj = HTTP.post(path, ["Content-Type" => "application/json"], file_contents)
+    obj = HTTP.post(path, ["Content-Type" => "application/json"], contents)
     return parse.(Int, split(chop(String(obj.body); head = 1, tail = 1), ","))
 end
 
