a0a242f6a710b07826f4ef6d32722043c1de22bf
TheCedarPrince <jacobszelko@gmail.com>
2021-11-04 15:06:53 -0700

    Added utilities and dependencies

--- julia/src/MentalHealthEquity.jl
+++ julia/src/MentalHealthEquity.jl
@@ -1,8 +1,13 @@
 module MentalHealthEquity
 
+using FunSQL
+using FunSQL: SQLTable, render
+using LibPQ
 using HTTP
 using JSON3
+using Tables
 
 include("atlasUtilities.jl")
+include("utilities.jl")
 
 end
