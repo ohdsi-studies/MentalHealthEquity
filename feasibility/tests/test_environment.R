01a680af7f2d4c2cb717a9762b743333d3506a4f
TheCedarPrince <jacobszelko@gmail.com>
2022-11-18 19:36:56 -0500

    Tests for feasibility environment assessment

---
+++ feasibility/tests/test_environment.R
@@ -0,0 +1,49 @@
+library(testthat)
+
+##############################################################
+# Loading test libraries
+##############################################################
+
+library(DatabaseConnector)
+library(Eunomia)
+library(SqlRender)
+
+##############################################################
+# Test variables for Eunomia Connection
+##############################################################
+
+cat("Testing Eunomia Connection Test Suite\n")
+
+eunomia_details <- Eunomia::getEunomiaConnectionDetails(databaseFile = "eunomia.sqlite")
+
+test_that("Eunomia Connection", {
+        expect_equal(object = length(eunomia_details), expected = 9)
+        expect_no_error(DatabaseConnector::connect(eunomia_details))
+})
+
+##############################################################
+# Test variables for queries
+##############################################################
+
+cat("\nTesting SqlRender Query Rendering Test Suite\n")
+
+rendered_query <- 'SELECT DISTINCT \"PERSON_1\".\"person_id\" FROM main.\"PERSON\" AS \"PERSON_1\"'
+
+test_that("Rendering queries", {
+        expect_equal(object = render('SELECT DISTINCT \"PERSON_1\".\"person_id\" FROM @schema.\"PERSON\" AS \"PERSON_1\"', schema = "main"), expected = rendered_query)
+})
+
+##############################################################
+# Test variables for Eunomia Connection
+##############################################################
+
+cat("\nTesting Eunomia Queries and Results\n")
+
+# Creating connection to Eunomia
+conn <- DatabaseConnector::connect(eunomia_details)
+
+query <- render('SELECT DISTINCT \"PERSON_1\".\"person_id\" FROM @schema.\"PERSON\" AS \"PERSON_1\" LIMIT 1', schema = "main")
+
+test_that("Running query", {
+        expect_equal(object = querySql(conn, query)$PERSON_ID, expected = 6)
+})
